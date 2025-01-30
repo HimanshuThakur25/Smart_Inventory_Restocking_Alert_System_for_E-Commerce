12cb04fbaf6903fccb195eb59ba999cee4775dde
HimanshuThakur25 <101442392+HimanshuThakur25@users.noreply.github.com>
2025-01-30 19:56:21 +0530

    Update settings.py

--- smart_inventory/settings.py
+++ smart_inventory/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'himanshu25t@gmail.com'
-EMAIL_HOST_PASSWORD = 'xxcs kqak ghdt skrw'
+EMAIL_HOST_USER = '###########@gmail.com'
+EMAIL_HOST_PASSWORD = '#### #### #### ####'
